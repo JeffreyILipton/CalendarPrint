b682ce2624372c78e387fa7edfa0c8d861884c64
Jeffrey Lipton <jeffreyilipton@gmail.com>
2013-03-25 16:22:06 -0400

    added datat to cal and made test file
    seems to work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CalendarInterface import getEvents
 from CalendarHelperFunctions import *
 from person import Person
+from MakeCookie import *
 
 
 
@@ -8,16 +9,16 @@ from person import Person
 
 guy = Person("jeff",25,240/2.2,(5*12+10)*2.54,'male')    
 
-eventList = getEvents('2013-03-08T00:00:00Z','2013-03-10T00:00:00Z')
-eventList.append(("ate:2000",60))
-eventList.append(("running",30))
+eventList = getEvents('2013-03-24T00:00:00Z','2013-03-25T00:00:00Z')
+#eventList.append(("ate:2000",60))
+#eventList.append(("running",30))
 calsEaten = eaten(eventList)
 calsBurned = burned(eventList,guy)
 
 guy.ate(calsEaten)
 guy.exercised(calsBurned)
 
-
+makeCookie(guy.calRemaining()*0.05)
 
 print eventList
 guy.printSelf()
